0c6fed781dcd089b5ea2e5626d289f2c8dabdf73
SilviMarku <marku.silvia777@gmail.com>
2023-10-09 13:20:41 +0000

    Squere

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
+class Square:
+    """
+    This is the documentation for the Square class.
+    The class represents a geometric square and associated operations.
+    """
+    pass
