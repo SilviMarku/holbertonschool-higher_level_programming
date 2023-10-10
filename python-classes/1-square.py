76080add07c806be50a48aaf7edc64691d92a69e
SilviMarku <marku.silvia777@gmail.com>
2023-10-10 12:23:16 +0000

    Squaree

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,9 +10,9 @@ class Square:
     This is the documentation for the Square class.
     The class represents a geometric square and associated operations.
     """
-    def __init__(self, size)
+    def __init__(self, size):
         """
-        python3 -c'print(__import__("my_module").my_function.__doc__)' 
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
         self.__size = size
