23fd0e3361266b2bbcb006c3d51644ab94619f85
SilviMarku <marku.silvia777@gmail.com>
2023-10-10 13:28:54 +0000

    Square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -20,3 +20,10 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.__size ** 2
