c21918dcc82b989ca1a635e22e0c4c71bdb23af7
SilviMarku <marku.silvia777@gmail.com>
2023-10-10 12:14:31 +0000

    Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,7 +7,7 @@ python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 
 class Square:
     """
-    This is the documentation for the Square class.
-    The class represents a geometric square and associated operations.
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
     """
     pass
