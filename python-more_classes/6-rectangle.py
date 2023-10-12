2f39184ff2f21a71dbf92f09d7c0d1b39baf6aeb
SilviMarku <marku.silvia777@gmail.com>
2023-10-12 12:32:12 +0000

    How many

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
+class Rectangle:
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * self.width + 2 * self.height
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        new_rec = []
+        for x in range(self.__height):
+            for y in range(self.__width):
+                new_rec.append("#")
+            if x < self.__height - 1:
+                new_rec.append("\n")
+        return "".join(new_rec)
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
