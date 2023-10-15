0a25c18d47d4923ee590736d4638e3b2b3061b88
SilviMarku <marku.silvia777@gmail.com>
2023-10-15 04:10:12 +0000

    Square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,12 +11,12 @@ class Square:
     The class represents a geometric square and associated operations.
     """
     def __init__(self, size=0):
+        self.__size = size
         """
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
