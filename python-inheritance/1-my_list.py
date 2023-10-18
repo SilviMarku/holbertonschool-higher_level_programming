e5bd9db850f826066ed175db7781077e4a6de91b
SilviMarku <marku.silvia777@gmail.com>
2023-10-18 12:49:52 +0000

    Inherit

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+class MyList(list):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+    def print_sorted(self):
+        """
+        'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        sorted_list = sorted(self)
+        print(sorted(self))
+        return sorted_list
+        
---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
+
+
+"""
+return isinstance(obj, a_class)
+"""
