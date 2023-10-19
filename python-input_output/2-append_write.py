7cc99b1d01e3b792ce5a2ed45688182b938208af
SilviMarku <marku.silvia777@gmail.com>
2023-10-19 13:15:57 +0000

    Apend

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,10 +4,10 @@ python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
     python3 -c 'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
