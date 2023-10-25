3b9aedc319ed9765d18879348a6aca580f2f9390
SilviMarku <marku.silvia777@gmail.com>
2023-10-25 13:55:03 +0000

    Rectangle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,15 +11,16 @@ class Base:
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-    """
-    python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-    """
-    if id != None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
