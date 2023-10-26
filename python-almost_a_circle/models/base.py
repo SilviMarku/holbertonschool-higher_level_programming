5a10c1ec663416846ceed3607e9a052de26a3579
SilviMarku <marku.silvia777@gmail.com>
2023-10-26 13:59:04 +0000

    Rectangle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,6 @@ class Base:
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -19,12 +18,12 @@ class Base:
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """
@@ -88,4 +87,4 @@ class Base:
             return l_inst
         except FileNotFoundError:
             return []
-                                
+            
