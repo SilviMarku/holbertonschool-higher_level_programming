fee555947b0bdc3e2831754aded78a9b82c3b685
SilviMarku <marku.silvia777@gmail.com>
2023-10-26 11:47:01 +0000

    Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,67 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+     @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        l_dict = []
+        if list_objs is not None:
+            for obj in list_objs:
+                l_dict.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", 'w') as f:
+            f.write(cls.to_json_string(l_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        file = cls.__name__ + ".json"
+        l_inst = []
+        try:
+            with open(file, "r") as f:
+                l_dict = Base.from_json_string(f.read())
+                for d_item in l_dict:
+                    inst = cls.create(**d_item)
+                    l_inst.append(inst)
+            return l_inst
+        except FileNotFoundError:
+            return []
