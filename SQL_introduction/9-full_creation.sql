3709e0bf2970055172563c218f1bbdd7a6a6924e
SilviMarku <marku.silvia777@gmail.com>
2023-11-21 07:06:35 -0800

    S

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT;
-	name Varchar(256),
+	name VARCHAR(256),
 	score INT
 	);
 
