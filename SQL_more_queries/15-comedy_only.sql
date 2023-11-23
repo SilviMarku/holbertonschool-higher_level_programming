08e62d651470d44c2c5f10d83420fdefc9aa4875
SilviMarku <marku.silvia777@gmail.com>
2023-11-23 07:34:44 -0800

    Sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT name FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY name ORDER BY name ASC;
+SELECT title FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title ORDER BY title ASC;
 
