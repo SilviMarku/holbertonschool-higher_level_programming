9f1d39a6d437ff789db920fd43ecb30950a97e50
SilviMarku <marku.silvia777@gmail.com>
2023-11-30 03:32:43 -0800

    Mapp

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -13,5 +13,4 @@ if __name__ == "__main__":
                 sys.argv[1], sys.argv[2], sys.argv[3]
                 ), pool_pre_ping=True
             )
-    Base.metadata.create_all(engine)
-
+    Base.metadata.create_all(engine);
