ea4561eb8f9934576bb76a8d4f79132eac7f1969
SilviMarku <marku.silvia777@gmail.com>
2024-02-02 15:28:54 +0100

    S

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+function turnRed() {
+    const kkoka = document.querySelector('header')
+    kkoka.style.color = '#FF0000'
+}
+
+document.getElementById('red_header')
+.addEventListener('click', turnRed)
